cf4746d404a83ff8c1153b7f0dbc52b017c67b63
lmjssjj <junjie.shi@itel-mobile.com>
2017-02-10 15:19:52 +0800

    first commit

---
+++ RecyclerViewDemo/src/com/lmjssjj/recyclerviewdemo/NameBean.java
@@ -0,0 +1,13 @@
+package com.lmjssjj.recyclerviewdemo;
+
+public class NameBean {
+	String name;  
+	  
+    public String getName() {  
+        return name;  
+    }  
+  
+    public void setName(String name) {  
+        this.name = name;  
+    }  
+}
